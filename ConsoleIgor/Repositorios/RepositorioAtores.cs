c3e71255368e671ae518769aac5647ef53ae1dc1
Yuri Hain <yuri.hain@messagecenter.com.br>
2022-07-28 12:44:14 -0300

    Gravar Ator

---
+++ ConsoleIgor/Repositorios/RepositorioAtores.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleIgor.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIgor.Repositorios
+{
+    public class RepositorioAtores
+    {
+        public RepositorioAtores()
+        {
+        }
+
+        public void GravarAtor(Ator ator)
+        {
+            //AQUI SIM O BANCO É CHAMADO PARA FAZER A INSERÇÃO, (SQL E PARAMETROS)
+            var banco = new Banco();
+
+            string sql = "insert into atores (nome, idade) values (@nome, @idade)";
+            banco.ExecuteNonQuery(sql, new Parametro("@nome", ator.Nome),
+                new Parametro("@idade", ator.Idade));
+        }
+    }
+}
--- WindowsForm/Form1.cs
+++ WindowsForm/Form1.cs
@@ -30,13 +30,19 @@ namespace WindowsForm
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            var banco = new Banco();
-            string sql = "INSERTO INTO atores (nome, idade) VALUES ('" + txtAtor + "' '" + txtIdade + "')";
-            banco.ExecuteNonQuery(sql);
+            //AQUI VOCÊ NÃO VAI MAIS CHAMAR O BANCO DIRETAMENTE, QUEM CHAMA A CLASSE BANCO AGORA É O REPOSITORIO DE ATORES
+            //COMENTEI SEU CÓDIGO ABAIXO
+            //var banco = new Banco();
+            //string sql = "INSERTO INTO atores (nome, idade) VALUES ('" + txtAtor + "' '" + txtIdade + "')";
+            //banco.ExecuteNonQuery(sql);
 
-
-
-
+            //CRIEI A CLASSE REPOSITORIO DE ATORES NA PASTA REPOSITORIOS
+            var repositorioAtores = new RepositorioAtores();
+            var ator = new Ator();
+            ator.Nome = txtAtor.Text;
+            ator.Idade = Convert.ToInt32(txtIdade.Text);
+            //AQUI ESTOU CHAMANDO UM MÉTODO DO REPOSITORIO ONDE PASSANDO O ATOR, ELE VAI INSERIR NO BANCO
+            repositorioAtores.GravarAtor(ator);
         }
     }
 }
